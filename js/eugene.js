7e779f4921a29b3f3873002020f4e0ec5cefbf61
Eugene Cross <eugene.cross@gmail.com>
2017-04-09 14:14:42 +0100

    Initial commit

---
+++ js/eugene.js
@@ -0,0 +1,23 @@
+
+function hoverIn() {
+  document.getElementById("profilecircle").className = "img-circle profileIn"
+}
+
+function hoverOut() {
+  document.getElementById("profilecircle").className = "img-circle profileOut"
+
+}
+
+var myVideo=document.getElementById("thevideo"); 
+
+function Pause() { 
+  document.getElementById("profilecircle").style.display="";
+  document.getElementById("thevideo").style.display="none";
+  myVideo.pause(); 
+}
+function Play() {
+  document.getElementById("profilecircle").style.display="none";
+  document.getElementById("thevideo").style.display="";
+if (myVideo.paused) 
+  myVideo.play();     
+}   
